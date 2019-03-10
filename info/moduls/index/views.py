ad0a73ceffb53e5db6c3d37aae4a51c4cb86f3d6
darcy <yanjiabinchn@gmail.com>
2019-03-10 20:17:22 +0800

    返回首页模板html 注意路径

--- info/moduls/index/views.py
+++ info/moduls/index/views.py
@@ -1,9 +1,12 @@
 from ..index import index_bp
 from info import redis_store
+# 导入视图模块
+from flask import render_template
+
 # 3使用蓝图装饰视图函数
 @index_bp.route('/')
 def index():
 
-    redis_store.setex("name", "laowang")
-
-    return "index"
+    redis_store.setex("name",10, "laowang")
+    # 指明 index.html 路径 注意js 路径
+    return render_template("news/index.html")
