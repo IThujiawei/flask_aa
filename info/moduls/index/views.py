f371ed7e0c28f73e0396b5fb32c7169ac1551b12
darcy <yanjiabinchn@gmail.com>
2019-03-10 22:26:46 +0800

    设置网站图标 ,使用 send_static_file

--- info/moduls/index/views.py
+++ info/moduls/index/views.py
@@ -1,7 +1,7 @@
 from ..index import index_bp
 from info import redis_store
 # 导入视图模块
-from flask import render_template
+from flask import render_template, current_app
 
 # 3使用蓝图装饰视图函数
 @index_bp.route('/')
@@ -10,3 +10,13 @@ def index():
     redis_store.setex("name",10, "laowang")
     # 指明 index.html 路径 注意js 路径
     return render_template("news/index.html")
+
+
+
+# 返回网站图标
+@index_bp.route("/favicon.ico")
+def favicon():
+
+    # 找到网站图片的静态资源返回浏览器
+    # current_app 获取当前 app 名称的
+    return current_app.send_static_file("news/favicon.ico")
